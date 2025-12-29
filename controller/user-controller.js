9936abd567ab0aa61988ea59f6aa4c8f57dd3f06
akojha556 <107338576+akojha556@users.noreply.github.com>
2025-12-29 21:44:26 +0530

    login section eroor handle if email or pw not there

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,27 +1,27 @@
-import User from "../model/user-model.js";
-import expressAsyncHandler from "express-async-handler";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import User from "../model/user-model.js";
+import expressAsyncHandler from "express-async-handler";
 
-//Register User
+//Register user
 export const registerUser = expressAsyncHandler(async (req, res) => {
      const { name, email, password } = req.body || {};
 
      if (!name || !email || !password) {
           res.status(400);
-          throw new Error("All field required");
-     }
+          throw new Error("Invalid user data");
+     };
 
-     //Check user exists
-     const userExists = await User.findOne({ email });
+     //Check if user exists or not
+     const user = await User.findOne({ email });
 
-     if (userExists) {
-          res.status(400);
-          throw new Error("User already exists, login");
-     }
+     if (user) {
+          res.status(409);
+          throw new Error("User already exists");
+     };
 
-     // hasing password
-     const salt = await bcrypt.genSalt(11);
+     // Hashing password
+     const salt = await bcrypt.genSalt(12);
      const hashedPassword = await bcrypt.hash(password, salt);
 
      const newUser = await User.create({
@@ -35,22 +35,27 @@ export const registerUser = expressAsyncHandler(async (req, res) => {
                _id: newUser.id,
                name: newUser.name,
                email: newUser.email
-          })
+          });
      } else {
           res.status(400);
-          throw new Error("Invalid User Data");
+          throw new Error("Invalid user-data");
      }
 });
 
-//Login User
+//Login user
 export const loginUser = expressAsyncHandler(async (req, res) => {
      const { email, password } = req.body || {};
 
-     //Check User exists or not
+     if (!email || !password) {
+          res.status(400);
+          throw new Error("Invalid user data");
+     };
+
+     //Check user exists or not
      const userExists = await User.findOne({ email });
 
      if (userExists && (await bcrypt.compare(password, userExists.password))) {
-          res.json({
+          res.status(200).json({
                _id: userExists.id,
                name: userExists.name,
                email: userExists.email
@@ -61,7 +66,7 @@ export const loginUser = expressAsyncHandler(async (req, res) => {
      }
 });
 
-//getMe
+//Get me
 export const getMe = expressAsyncHandler(async (req, res) => {
      res.status(200).json({ message: "Get me" });
 });
